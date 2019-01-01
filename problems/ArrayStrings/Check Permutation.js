61b991acab631b9135e25bfeaea32fae10acbc58
Alfred Datui <alfred1datui@gmail.com>
2019-01-01 14:17:00 +0800

    looks like reducing edge case line to more general check is better

--- problems/ArrayStrings/Check Permutation.js
+++ problems/ArrayStrings/Check Permutation.js
@@ -1,4 +1,9 @@
 // Check Permutation: Given two strings, write a method to decide if one is a permutation of the other
+//I will ask the ASCII or Unicode.
+// - Lowercase and uppercase?
+// - Talk about the tradeoff
+//     - Space?? : sort them both and compare from left to right
+//     - Time??: : need extra space though
 
 function checkPermutation(s1, s2) {
   const ln1 = s1.length;
@@ -15,15 +20,8 @@ function checkPermutation(s1, s2) {
   }
 
   for (let i = 0; i < ln2; i++) {
-    if (arr[s2[i].charCodeAt(0)] == 0) {
-      return false;
-    }
-
     arr[s2[i].charCodeAt(0)]--;
-  }
-
-  for (let i = 0; i < 128; i++) {
-    if (arr[i] != 0) {
+    if (arr[s2[i].charCodeAt(0)] < 0) {
       return false;
     }
   }
@@ -32,6 +30,6 @@ function checkPermutation(s1, s2) {
 }
 
 const input1 = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./asdfghjkl;'";
-const input2 = "/.,mnbvcxz';lkjhgfdsa\\][poiuytrewq=-0987654321`';lkjhgfdsas";
+const input2 = "/.,mnbvcxz';lkjhgfdsa\\][poiuytrewq=-0987654321`';lkjhgfdsa";
 
 console.log(checkPermutation(input1, input2));
