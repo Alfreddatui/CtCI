af4c99d33e3c93c05216ab861a899eb32ad81f0e
Alfred Datui <alfred1datui@gmail.com>
2019-01-18 16:46:13 +0800

    implement inorder, preorder and postorder

--- data-structures/binary-search-tree.js
+++ data-structures/binary-search-tree.js
@@ -49,6 +49,10 @@ class binaryTree {
     this.recPrint(node.right, total);
     total[0] += ")";
   }
+
+  getRoot() {
+    return this.root;
+  }
 }
 
 class Node {
@@ -59,11 +63,104 @@ class Node {
   }
 }
 
+function inOrder(node) {
+  if (node == null) {
+    return;
+  }
+  console.log(node.value);
+  inOrder(node.left);
+  inOrder(node.right);
+}
+
+function preOrder(node) {
+  if (node == null) {
+    return;
+  }
+  preOrder(node.left);
+  console.log(node.value);
+  preOrder(node.right);
+}
+
+function postOrder(node) {
+  if (node == null) {
+    return;
+  }
+  postOrder(node.left);
+  postOrder(node.right);
+  console.log(node.value);
+}
 
 let bt = new binaryTree();
-bt.insert(1);
-bt.insert(2);
-bt.insert(1.5);
-bt.insert(99);
-bt.insert(0);
+bt.insert(32);
+bt.insert(15);
+bt.insert(11);
+bt.insert(17);
+bt.insert(10);
+bt.insert(12);
+bt.insert(14);
+bt.insert(16);
+bt.insert(20);
+
+bt.insert(46);
+bt.insert(36);
+bt.insert(58);
+bt.insert(34);
+bt.insert(40);
+bt.insert(50);
+bt.insert(100);
 console.log(bt.printTree());
+
+const root = bt.getRoot();
+// inOrder(root);
+// 32
+// 15
+// 11
+// 10
+// 12
+// 14
+// 17
+// 16
+// 20
+// 46
+// 36
+// 34
+// 40
+// 58
+// 50
+// 100
+
+// preOrder(root);
+// 10
+// 11
+// 12
+// 14
+// 15
+// 16
+// 17
+// 20
+// 32
+// 34
+// 36
+// 40
+// 46
+// 50
+// 58
+// 100
+
+postOrder(root);
+// 10
+// 14
+// 12
+// 11
+// 16
+// 20
+// 17
+// 15
+// 34
+// 40
+// 36
+// 50
+// 100
+// 58
+// 46
+// 32
